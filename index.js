3dbe59385b59e56201e86c970967f2ec7b9b93ab
mahabub alam <mahabu998@gmail.com>
2021-04-28 01:59:38 +0600

    heroku solved

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId
 const port = process.env.PORT || 5000;
@@ -9,7 +10,8 @@ require('dotenv').config();
 
 app.use(express.json());
 app.use(cors());
-app.use(express.urlencoded({extended: false }));
+app.use(bodyParser.json())
+// app.use(express.urlencoded({extended: false }));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dwn45.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
